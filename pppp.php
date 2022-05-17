9f29891a636f420a14d7998e330d119ae72e2290
PabloHO25 <78091713+PabloHO25@users.noreply.github.com>
2022-05-17 13:29:20 -0500

    Add files via upload

---
+++ pppp.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=	, initial-scale=">
+	<title>Document</title>
+</head>
+
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+echo utf8_decode("l2ñññ");
+?>
+
+
+<p> hola!!!nnnnnñññññño´x'i'oóóó</p>
+<body>
+	
+</body>
+</html>
